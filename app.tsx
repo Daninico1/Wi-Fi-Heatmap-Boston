cb8a6e330de302e91342923d6b00fcbf5a0204c8
Daninico1 <62447071+Daninico1@users.noreply.github.com>
2026-07-24 13:09:27 -0400

    Change map style to dark matter

--- app.tsx
+++ app.tsx
@@ -46,7 +46,7 @@ const INITIAL_VIEW_STATE: MapViewState = {
   bearing: 0
 };
 
-const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
+const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json';
 
 export const colorRange: Color[] = [
   [1, 152, 189],
